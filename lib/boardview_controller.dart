5f3257c040ff675b5ec8ef879e8d5d0359737597
quang.le <quang.le@cleeksy.com>
2023-11-17 11:45:40 +0700

    Update animateTo page index

--- lib/boardview_controller.dart
+++ lib/boardview_controller.dart
@@ -10,9 +10,11 @@ class BoardViewController {
   Future<void> animateTo(int index, {Duration? duration, Curve? curve}) async {
     double offset = index * (state.widget.width + state.widget.margin!);
     if (state.boardViewController.hasClients) {
+      state.canDrag = false;
       await state.boardViewController.animateTo(offset,
           duration: duration ?? new Duration(milliseconds: 400),
           curve: curve ?? Curves.ease);
+      state.canDrag = true;
     }
   }
 }
